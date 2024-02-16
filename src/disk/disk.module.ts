3693edc8b5f1c9976f1016f994a163ad127cb4b9
markpakingan <116990353+markpakingan@users.noreply.github.com>
2024-02-16 08:14:29 -0800

    connected cpu and disk module to computer module

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { DiskService } from './disk.service';
 import { PowerModule } from 'src/power/power.module';
 
-
 @Module({
-    imports: [PowerModule],
-    providers: [DiskService],
+  imports: [PowerModule],
+  providers: [DiskService],
+  exports: [DiskService],
 })
 export class DiskModule {}
